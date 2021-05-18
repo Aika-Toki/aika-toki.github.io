45b101edffeac1be5de43b94d43b04351516fd72
kote-419 <53148052+kote-419@users.noreply.github.com>
2021-05-18 23:20:17 +0900

    #67

---
+++ sites/redirect/login/login.js
@@ -0,0 +1,6 @@
+if (window.location.hash) {
+    alert("無効なURLです.")
+    window.location.replace("https://aika-toki.github.io");
+} else {
+    window.location.replace("https://aika-toki.github.io/sites/redirect/#eyJ2IjoiMC4wLjEiLCJlIjoiR0FDYVhTZ0hvanhtVXh6SzduTkxLRmREOWo5czdqdHJ4RUt0SUkwand0OGRDNGQwaHdQeVlKNEpTbHFQQjlxN3U1blNqajJ4NTRtVjZ6NGhWZnBDVjB4MUJRPT0iLCJzIjoiZEhsUnpyRzBsazN4TnNxa29uVVJEUT09IiwiaSI6IklJaUxuY1NneFZkWnppTVMifQ==");
+}
