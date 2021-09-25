60b7163bf8a6bd2350c8583a0afb0fe7a703dc3e
aika-toki <83457227+Aika-Toki@users.noreply.github.com>
2021-09-25 16:30:36 +0900

    update

--- sites/sakura_capture/js/script.js
+++ sites/sakura_capture/js/script.js
@@ -1,4 +1,4 @@
-let d = document,curl,ciframe,cconf;
+let d = document,curl,ciframe,cid,cconf,cmainb,creportb;
 window.onload = function(){
     curl = d.querySelector('#sakuraUrl');
     ciframe = d.querySelector('#sakuraCheckerSite');
@@ -7,4 +7,19 @@ window.onload = function(){
 function load() {
     let id = curl.value;
     ciframe.setAttribute('src', `https://sakura-checker.jp/search/${id}/`);
+    cid = ciframe.contentWindow.document;
+    cmainb = cid.querySelector('.mainBlock');
+    creportb = cid.querySelector('.reportBlock');
+    html2canvas(cmainb,{
+        onrendered: function(canvas){
+            var imgData = canvas.toDataURL();
+            document.getElementById("resultMain").src = imgData;
+        }
+    });
+    html2canvas(creportb,{
+        onrendered: function(canvas){
+            var imgData = canvas.toDataURL();
+            document.getElementById("resultReport").src = imgData;
+        }
+    });
 }
