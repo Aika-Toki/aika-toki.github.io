7791fce0f2845ed652c4b275756b256c4b06acf0
aika-toki <83457227+Aika-Toki@users.noreply.github.com>
2022-07-13 15:30:33 +0900

    02220b

--- others/javascript/Dolpthone_inject.js
+++ others/javascript/Dolpthone_inject.js
@@ -1,7 +1,4 @@
 window_5209hbf298hew9jfiowg_Dolpthone_version = "0.2.220 Beta";
-let swalsc = window.top.document.createElement('script');
-swalsc.src = "//cdn.jsdelivr.net/npm/sweetalert2@11";
-window.top.document.querySelector('script#di').before(swalsc);
 startup();
 subscribe();
 function subscribe() {
@@ -18,47 +15,53 @@ function done(){
             console.log("Next")
         },
         1000
-    );
-    let sub = setTimeout(
-        ()=>{
-            subscribe();
-            reinject();
-            console.log("Subscribe");
-            clearTimeout(sub);
-        },
-        10000
-    );
-}
-function next() {
-    const caps = window.top.document.getElementsByClassName("u-list")[0].getElementsByTagName("li"); 
-
-    let lastIndex = 0
-    for (const item in caps) { 
-        const cl = caps[item].classList
-        if (!cl.contains("good")) {
-            if (cl.contains("movie") && !cl.contains("supplement")) {
-                break;
-            } else if (cl.contains("evaluation-test") || cl.contains("essay-test")) {
-                let notifsound = new Audio("https://aika-toki.github.io/others/library/NotiSound/002_3s.wav");
-                notifsound.play();
-                notifsound.addEventListener('ended',()=>{
-                    notifsound = "";
-                });
+        );
+        let sub = setTimeout(
+            ()=>{
+                subscribe();
+                reinject();
+                console.log("Subscribe");
+                clearTimeout(sub);
+            },
+            10000
+            );
+        }
+        function next() {
+            const caps = window.top.document.getElementsByClassName("u-list")[0].getElementsByTagName("li"); 
+            
+            let lastIndex = 0
+            for (const item in caps) { 
+                const cl = caps[item].classList
+                if (!cl.contains("good")) {
+                    if (cl.contains("movie") && !cl.contains("supplement")) {
+                        break;
+                    } else if (cl.contains("evaluation-test") || cl.contains("essay-test")) {
+                        let notifsound = new Audio("https://aika-toki.github.io/others/library/NotiSound/002_3s.wav");
+                        notifsound.play();
+                        notifsound.addEventListener('ended',()=>{
+                            notifsound = "";
+                        });
                 break;
             }
         }
         lastIndex += 1
     }
-
+    
     caps[lastIndex].querySelector("a").click();
 }
 function startup(){
+    let swalsc = window.top.document.createElement('script');
+    swalsc.src = "//cdn.jsdelivr.net/npm/sweetalert2@11";
+    window.top.document.querySelector('script#di').before(swalsc);
     let version = window_5209hbf298hew9jfiowg_Dolpthone_version;
     console.log('\u001b[33m[Dolpthone]Ver.'+version+' (re)loaded.');
-    let version_sentense = `Dolpthone version ${version} has applied.`;
     let patchsound = new Audio('https://aika-toki.github.io/others/library/NotiSound/001_2s.wav');
     patchsound.play();
-    window.opener.Swal.fire(version_sentense);
+    window.opener.swalalart(version);
+}
+function swalalart(version){
+    let version_sentense = `Dolpthone version ${version} has applied.`;
+    Swal.fire(version_sentense);
 }
 function reinject(){
     let p=document.querySelector('script#di');
