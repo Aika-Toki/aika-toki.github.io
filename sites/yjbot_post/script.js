23b81c8f2d8d0b53cb89d0f1728c3372f39c88a7
aika-toki <83457227+Aika-Toki@users.noreply.github.com>
2022-07-21 21:33:12 +0900

    modified:   sites/yjbot_post/index.html modified:   sites/yjbot_post/script.js

--- sites/yjbot_post/script.js
+++ sites/yjbot_post/script.js
@@ -1,7 +1,7 @@
-function mdlgen(c){
+function mdlgen(c,t){
     let r = str264(c);
     r = "https://aika-toki.github.io/sites/DiscordBotAnnouncement/?md="+r;
     console.log(r);
     r = exurlconv(r);
-    dqs('#resultmdl').value = r;
+    dqs('#resultmdl').value = `<announce-web _ ${t} ${r}`;
 }
