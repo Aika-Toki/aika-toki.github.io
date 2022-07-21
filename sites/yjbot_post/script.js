4f0840908b5cd54ba3512200a395962b348c8f6c
aika-toki <83457227+Aika-Toki@users.noreply.github.com>
2022-07-21 15:46:45 +0900

    modified:   sites/yjbot_post/index.html modified:   sites/yjbot_post/script.js

--- sites/yjbot_post/script.js
+++ sites/yjbot_post/script.js
@@ -1,5 +1,6 @@
 function mdlgen(c){
     let r = str264(c);
+    r = "https://aika-toki.github.io/sites/DiscordBotAnnouncement/?md="+r;
     console.log(r);
     r = exurlconv(r);
     dqs('#resultmdl').value = r;
